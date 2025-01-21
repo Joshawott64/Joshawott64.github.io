5e8c7083fabe0dff74abd16b327ea775083df4d9
Joshua Gray <graydog.8719@gmail.com>
2025-01-20 23:00:59 -0700

    implemented error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,38 @@
+import { useNavigate, useRouteError } from "react-router-dom";
+
 const ErrorPage = () => {
+  const navigate = useNavigate();
+  const error = useRouteError();
+
+  console.error(error);
+
   return (
     <>
-      <p>Error Page</p>
+      <div className="flex flex-col justify-center place-items-center h-svh w-full text-text-light bg-gradient-to-b from-lightest to-dark to-[130%] drop-shadow-lg">
+        <div className="flex flex-col justify-center place-items-center w-72 h-1/2 bg-lightest rounded-3xl p-6 drop-shadow-lg">
+          <div className="flex-1 flex flex-col justify-center place-items-center text-center">
+            <p className="text-dark-accent text-5xl font-semibold drop-shadow-lg">
+              {error.status}
+            </p>
+            <p className="text-4xl font-semibold drop-shadow-lg">-</p>
+            <p className="text-2xl font-semibold drop-shadow-lg">
+              {error.statusText}
+            </p>
+          </div>
+          <div className="flex-1 flex flex-col justify-center place-items-center text-center">
+            <p className="text-center drop-shadow-lg">Error:</p>
+            <p className="text-center drop-shadow-lg">{error.error.message}</p>
+          </div>
+          <div className="flex-1 flex flex-col justify-center place-items-center text-center drop-shadow-lg">
+            <p
+              onClick={() => navigate("/")}
+              className="text-lightest bg-dark bg-opacity-90 p-4 border-2 border-dark rounded-full cursor-pointer hover:border-darkest drop-shadow-lg transition-all duration-300"
+            >
+              Return to home page
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
